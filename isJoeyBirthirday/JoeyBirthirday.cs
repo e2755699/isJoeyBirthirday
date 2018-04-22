8f07482b09429ef263988b79fce1c5fa01cd0d4b
dustin.liu62 <dustin.liu62@TI03BY45>
2018-04-22 18:57:22 +0800

    complelte the code

---
+++ isJoeyBirthirday/JoeyBirthirday.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace isJoeyBirthday
+{
+    public class JoeyBirthirday
+    {
+        protected DateTime _dateTime;
+
+        public JoeyBirthirday()
+        {
+            _dateTime = DateTime.Today;
+        }
+
+        public JoeyBirthirday(DateTime date)
+        {
+            _dateTime = date;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string isBirthday()
+        {
+            GetDate();
+
+            if(_dateTime.ToString("MM/dd") == "11/28")
+            {
+                return "Happy Birthday";
+            }
+            return "No, Sorry";
+        }
+
+        protected virtual void GetDate()
+        {
+            _dateTime = DateTime.Today;
+        }
+
+    }
+}
--- isJoeyBirthirday/UnitTest1.cs
+++ isJoeyBirthirday/UnitTest1.cs
@@ -1,14 +1,49 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
-namespace isJoeyBirthirday
+namespace isJoeyBirthday
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsJoeyBirtdayShouldBeReturnHannpyBirthday()
         {
+            //arrang
+            var date = new DateTime(2018, 11, 28);
+            var jeoyBirtday = new FakeJoey();
+            jeoyBirtday.setDate(date);
+            //act
+            var result = jeoyBirtday.isBirthday();
+            //assert
+            Assert.AreEqual("Happy Birthday", result);
+        }
+
+        [TestMethod]
+        public void NotIsJoeyBirtdayShouldBeReturnHannpyBirthday()
+        {
+            //arrang
+            var date = new DateTime(2018, 11, 27);
+            var jeoyBirtday = new JoeyBirthirday(date);
+            //act
+            var result = jeoyBirtday.isBirthday();
+            //assert
+            Assert.AreEqual("No, Sorry", result);
+        }
+    }
+
+    public class FakeJoey : JoeyBirthirday
+    {
+        private DateTime _date;
+
+        internal void setDate(DateTime date)
+        {
+            _date = date;
+        }
+
+        protected override void GetDate()
+        {
+            _dateTime = _date;
         }
     }
 }
